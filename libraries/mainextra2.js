3e46bab75ca6b05c5fc9010c997dd081b80820b6
mquang <mqfrontendtime@gmail.com>
2020-11-05 22:39:29 +0700

    ...

--- libraries/mainextra2.js
+++ libraries/mainextra2.js
@@ -694,7 +694,7 @@ function arrayShuffle () {
 Array.prototype.shuffle =arrayShuffle;
     
 var start = 1;
-var end = 67;
+var end = 69;
 var numbers = new Array(); 
 for (var i = start; i <= end; i++) {
     numbers.push(i);
